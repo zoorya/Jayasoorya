0a1c0bd25e5c3fdc7b10c0dc2c4c6866a0b8fa1d
zoorya <80897736+zoorya@users.noreply.github.com>
2023-01-20 10:32:13 +0530

    Update Welcome.js

--- Welcome.js
+++ Welcome.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Welcome = ({name, regNo}) => {
     return (
         <div>
-            <h1>Hey hello! {name}</h1>
-            <h2> we Welcome you  to LPU College</h2>
+            <h1> Hey {name}</h1>
+            <h2>  Welcome to LPU College</h2>
         </div>
     )
 }
